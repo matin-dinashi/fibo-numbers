36e3e3175aa24fc89059d1eb401e910804f773f6
matin-dinashi <matindinashi@outlook.com>
2021-04-10 17:28:50 +0430

    Initial and Done Project

---
+++ FibonacciNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Enter Count Of Your Fibonacci Numbers : ");
+            string countOfNumbers = Console.ReadLine();
+            Console.ResetColor();
+            bool isValid = Validation.ValidateCountOfNumbers(countOfNumbers);
+            if (isValid)
+            {
+                var fibonacciNumbers = new List<int> { 1, 1 };
+
+                while (fibonacciNumbers.Count < int.Parse(countOfNumbers)) {
+                    int previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                    int previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+                    fibonacciNumbers.Add(previous + previous2);
+                }
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Numbers : ");
+                foreach (var item in fibonacciNumbers) {
+                    Console.WriteLine(item);
+                }
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Run Program Again and Enter a Valid Number");
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ FibonacciNumbers/Validation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FibonacciNumbers
+{
+    public class Validation
+    {
+        public static bool ValidateCountOfNumbers(string count)
+        {
+            try
+            {
+                int countOfNumbers = int.Parse(count);
+                if (countOfNumbers <= 1)
+                {
+                    return false;
+                }
+                else if (countOfNumbers >= int.MaxValue)
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            
+            return true;
+        }
+    }
+}
